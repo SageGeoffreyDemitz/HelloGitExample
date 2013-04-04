c1cd9cd53cdc4d2cbf7ff1d5c173a78442a2fa8f
Geoffrey Demitz <geoffrey.demitz@sage.com>
2013-04-04 15:11:13 -0400

    Added random fortune

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -11,8 +11,29 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Git!");
-            Console.WriteLine("You've been modified!"); 
+            Console.WriteLine("You've been modified!");
+            Console.WriteLine("Today's Fortune: " + GetFortune()); 
             Console.ReadKey(); 
+        }
+
+        private static  string GetFortune()
+        {
+            Random rnd = new Random();
+            int whichOne = rnd.Next(3);
+            switch (whichOne)
+            {
+                case 1:
+                    return "You will meet a tall dark stranger.";
+                    
+                case 2:
+                    return "No bugs!!!";
+                    
+                case 3:
+                    return "Avoid dogs today.";
+                    
+                default:
+                    return "No fortune";
+            }
         }
     }
 }
